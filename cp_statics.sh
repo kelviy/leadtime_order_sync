963727d61793258f326ef3b071505f8da62922ab
kelviy <52874397+kelviy@users.noreply.github.com>
2025-03-22 11:33:27 +0200

    Added script to cp javascript to static

---
+++ cp_statics.sh
@@ -0,0 +1,4 @@
+# to be run from plugin folder (leadtime_order_sync) from plugins folder for docker installation
+mkdir -p ../../static/leadtime_order_sync
+cp ./static/leadtime_order_sync/leadtime_order_sync.js ../../static/leadtime_order_sync/
+
